50b9524f3919b709690e3ea07c30e749c4d69d4c
alfaizuna <alfaizunaulia@gmail.com>
2026-01-13 14:38:27 +0700

    fix: handle empty name in Hello function
    Add default "World" value when name is empty in Hello function and update tests to verify this behavior

--- helloyou/hello_test.go
+++ helloyou/hello_test.go
@@ -4,10 +4,23 @@ import (
 	"testing"
 )
 
-func TestHelo(t *testing.T) {
-	got := Hello("Alfa")
-	want := "Hello, Alfa"
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Alfaizuna")
+		want := "Hello, Alfaizuna"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
