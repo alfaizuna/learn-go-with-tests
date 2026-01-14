aaa67b5d3a8e6af07733d136bcf68a13b62b8034
alfaizuna <alfaizunaulia@gmail.com>
2026-01-14 21:07:46 +0700

    feat: add sum function for arrays with test
    Implement basic array summation functionality with corresponding test case

---
+++ arraysandslices/sum_test.go
@@ -0,0 +1,13 @@
+package arraysandslices
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [5]int{1,2,3,4,5}
+	got := Sum(numbers)
+	want := 15
+
+	if got != want {
+		t.Errorf("got %q want %q given, %q", got, want, numbers)
+	}
+}
